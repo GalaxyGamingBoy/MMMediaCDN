3698fea378772c6b0f54aa3b868e5b490e3e578e
GalaxyGamingBoy <xrteach@gmail.com>
2021-11-28 21:04:27 +0200

    Update

--- js/technologiaSelector.js
+++ js/technologiaSelector.js
@@ -1,6 +1,11 @@
 var documentBTN20211025 = document.getElementById("documentBTN20211025");
+var documentBTN20211128 = document.getElementById("documentBTN20211128");
 documentBTN20211025.addEventListener('click', function () {
     console.log("documentBTN20211025 Clicked!");
     window.open("Documents/Technologia/20211025.html", "_self");
+});
+documentBTN20211128.addEventListener('click', function () {
+    console.log("documentBTN20211128 Clicked!");
+    window.open("Document/Technologia/20211128.html", "_self");
 });
 console.log("technologiaSelector Script loaded!");
