4520c037105c171afd8f8976f915398cebe2df82
GalaxyGamingBoy <53054461+GalaxyGamingBoy@users.noreply.github.com>
2021-11-28 21:27:15 +0200

    Delete ts directory

--- ts/allaSelector.ts
+++
@@ -1,8 +0,0 @@
-const timetable = document.getElementById("timetable") as HTMLButtonElement;
-
-timetable.addEventListener('click', ()=>{
-    console.log("documentBTN20211025 Clicked!");
-    window.open("Documents/Alla/Timetable.html", "_self");
-});
-
-console.log("allaSelector Script loaded!");
