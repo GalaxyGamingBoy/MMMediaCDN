3b82135064f07eabe6395eae6d8a3aefbfe89e3d
GalaxyGamingBoy <53054461+GalaxyGamingBoy@users.noreply.github.com>
2021-11-28 21:27:02 +0200

    Delete js directory

--- js/allaSelector.js
+++
@@ -1,6 +0,0 @@
-var timetable = document.getElementById("timetable");
-timetable.addEventListener('click', function () {
-    console.log("documentBTN20211025 Clicked!");
-    window.open("Documents/Alla/Timetable.html", "_self");
-});
-console.log("allaSelector Script loaded!");
